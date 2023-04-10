994846009af443860fe9bb6b30faba48b36432b0
Amandeep Kaur <000822179@mohawkcollege.ca>
2023-04-10 07:34:55 -0400

    Created Book Object

---
+++ app/src/main/java/com/example/android_books_library/Book.java
@@ -0,0 +1,9 @@
+package com.example.android_books_library;
+
+public class Book {
+    private String title;
+    private String author;
+    private String imageUrl;
+    // getters and setters
+}
+
